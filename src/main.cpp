a6672e504b29ea159a3b79514a4d9e3d7e0aa273
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-04-25 11:22:49 +0200

    Add lua

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "LuaCpp.hpp"
 #include "index.h"
 #include "ninja.h"
 #include <filesystem>
@@ -5,6 +6,17 @@
 
 int main(int argc, char *argv[]) {
     std::ios::sync_with_stdio(false);
+
+    auto lua = LuaCpp::LuaContext{};
+
+    // The simples way is to use CompileStringAndRun method
+    try {
+        lua.CompileStringAndRun(
+            "print('The fastest way to start using lua in a project')");
+    }
+    catch (std::runtime_error &e) {
+        std::cout << e.what() << '\n';
+    }
 
     auto index = Index{};
 
