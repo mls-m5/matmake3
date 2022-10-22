9f9a8e6812cfacd7a8d822dad217e75d7c27d19a
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-10-22 08:51:17 +0200

    Add description of possible workaround

--- demos/05-import-includes/apa.cpp
+++ demos/05-import-includes/apa.cpp
@@ -1,8 +1,11 @@
-import "apa.h";
+// A workaround when you want to import iostream and string is to only import
+// string
+
 import <iostream>;
-// import <string>; // Causes an error
+// import <string>;
 
 int print() {
     std::cout << "hello" << std::endl;
+    std::string str = "satnhoeu";
     return 10;
 }
