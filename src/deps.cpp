c8847d5cd9673517e44fab63f5f6a4275df37922
Mattias Lasersköld <mattias.l.sk@gmail.com>
2023-01-27 15:45:00 +0100

    Simplify dep searching

--- src/deps.cpp
+++ src/deps.cpp
@@ -5,15 +5,14 @@
 using std::literals::operator""sv;
 
 std::vector<std::string> parseModuleDeps(std::filesystem::path path) {
-    // Todo: Maybe cache deps in intermediate dep file
-    auto deps = std::vector<std::string>{};
+    // TODO: Maybe cache deps in intermediate dep file
 
-    auto file = std::ifstream{path};
-
-    auto importStr = "import "sv;
-    auto moduleStr = "module "sv;
-    auto exportImportStr = "export import "sv;
-    auto includeStr = "#include "sv;
+    auto strings = std::array{
+        "import "sv,
+        "module "sv,
+        "export import "sv,
+        "#include "sv,
+    };
 
     auto findImport = [](const std::string &line,
                          std::string_view type) -> std::string {
@@ -30,6 +29,9 @@ std::vector<std::string> parseModuleDeps(std::filesystem::path path) {
         return dep;
     };
 
+    auto deps = std::vector<std::string>{};
+    auto file = std::ifstream{path};
+
     for (std::string line; std::getline(file, line);) {
         if (line.empty()) {
             continue;
@@ -37,21 +39,11 @@ std::vector<std::string> parseModuleDeps(std::filesystem::path path) {
         if (line.back() == '\r') {
             line.pop_back();
         }
-        if (auto dep = findImport(line, importStr); !dep.empty()) {
-            deps.push_back(dep);
-            continue;
-        }
-        if (auto dep = findImport(line, moduleStr); !dep.empty()) {
-            deps.push_back(dep);
-            continue;
-        }
-        if (auto dep = findImport(line, exportImportStr); !dep.empty()) {
-            deps.push_back(dep);
-            continue;
-        }
-        if (auto dep = findImport(line, includeStr); !dep.empty()) {
-            deps.push_back(dep);
-            continue;
+        for (auto str : strings) {
+            if (auto dep = findImport(line, str); !dep.empty()) {
+                deps.push_back(dep);
+                break;
+            }
         }
     }
 
